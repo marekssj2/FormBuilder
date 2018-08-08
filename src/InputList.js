1fd03cfaddbb638e6c5f13ac32c3a37a7030b618
marekssj2 <marekssj2@gmail.com>
2018-08-08 10:23:05 +0200

    Css updates

--- src/InputList.js
+++ src/InputList.js
@@ -34,7 +34,6 @@ class InputList extends Component {
     this.setState({
       inputList: listArray
     });
-    console.log(this.state);
   };
 
   updateInputComponent = (id, type, value) => {
@@ -48,21 +47,21 @@ class InputList extends Component {
 
   getDescendantIds = id => {
     const input = this.getInput(id);
-    let ids = [id]; // Co właściwie daje to id wewnątrz listy? - [id]
+    let ids = [id];
 
     if (input.children.length > 0) {
       input.children.forEach(child => {
         ids = ids.concat(this.getDescendantIds(child));
       });
     }
-    return ids; //zwracasz listę z id wszystkich inputów które są potomkami danego inputa
+    return ids;
   };
 
   deleteInputComponent = id => {
-    let inputList = this.state.inputList; //robimy kopię listy inputów
+    let inputList = this.state.inputList;
     const parentIndex = inputList.findIndex(input =>
       input.children.includes(id)
-    ); // Co tu się dzieje? wiem że szukamy w liście z wszystkimi inputami indeksum, tylko co dzieje się w tej funkcji strzałkowej? , ale czy mógł byś troszeczkę to przybliżyć?
+    );
 
     this.getDescendantIds(id).forEach(id => {
       inputList = inputList.filter(input => input.id !== id);
@@ -78,15 +77,15 @@ class InputList extends Component {
     });
   };
 
-  // deleteInputComponent = id => {
-  //   const listArray = this.state.inputList;
-  //   let curList = listArray.filter(item => item.id !== id);
+  // deleteInputComponent = (id) => {
+  //     const listArray = this.state.inputList
+  //     let curList = listArray.filter(item => item.id !== id )
 
-  //   this.setState({
-  //     inputList: curList
-  //   });
-  //   console.log(curList);
-  // };
+  //     this.setState({
+  //         inputList: curList
+  //     })
+  //     console.log(curList)
+  // }
 
   getInput = id => {
     return this.state.inputList.find(item => item.id === id);
@@ -96,6 +95,7 @@ class InputList extends Component {
     const { inputList } = this.state;
     return (
       <div className="App">
+        <h1>Form Builder</h1>
         {inputList.map(item => {
           if (item.parentId === null) {
             return (
